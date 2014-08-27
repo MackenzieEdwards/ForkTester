40ce9c7198117c751fb8b2a632d19677247dd5ef
Mackenzie <Mackenzie@house-dell.domain.actdsltmp>
2014-08-27 08:15:17 -0600

    Home

--- Fork Tester/src/ForkTester.java
+++ Fork Tester/src/ForkTester.java
@@ -4,6 +4,7 @@ public class ForkTester
 	public static void main(String[] args)
 		{
 		System.out.println("School");
+		System.out.println("Home");
 		// Try what you did before.  
 		// Fork this to your personal GitHub account.
 		// Download it to school and add a line.
